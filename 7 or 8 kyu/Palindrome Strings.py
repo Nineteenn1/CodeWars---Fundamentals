01b182b1c3177ab4c997632525ed6f4221d0cf36
fxxsx <83366051+fxxsx@users.noreply.github.com>
2022-04-29 12:50:27 +0200

    Create Palindrome Strings.py

---
+++ 7 or 8 kyu/Palindrome Strings.py
@@ -0,0 +1,13 @@
+'''
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+
+Examples
+isPalindrome("anna")   ==> true
+isPalindrome("walter") ==> false
+isPalindrome(12321)    ==> true
+isPalindrome(123456)   ==> false
+'''
+def is_palindrome(string):
+    return str(string) == str(string)[::-1]
