3294aaee0c6b5102f1647d6a16f0e1d07032055e
fxxsx <83366051+fxxsx@users.noreply.github.com>
2022-05-06 15:36:28 +0200

    Made it shorter

--- 7 or 8 kyu/Convert boolean values to strings 'Yes' or 'No'..py
+++ 7 or 8 kyu/Convert boolean values to strings 'Yes' or 'No'..py
@@ -2,6 +2,4 @@
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 '''
 def bool_to_word(boolean):
-    if str(boolean) == 'True':
-        return 'Yes'
-    else: return 'No'
+   return "Yes" if str(boolean) == 'True' else 'No'
