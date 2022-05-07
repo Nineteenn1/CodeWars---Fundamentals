a690cd1e8bcf4a5a2ee7838ca63a9fdba5469d52
fxxsx <83366051+fxxsx@users.noreply.github.com>
2022-05-07 19:07:00 +0200

    Create Twice as old.py

---
+++ 7 or 8 kyu/Twice as old.py
@@ -0,0 +1,10 @@
+"""
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+"""
+def twice_as_old(dad_years_old, son_years_old):
+    return abs(dad_years_old - 2 * son_years_old)
+  
